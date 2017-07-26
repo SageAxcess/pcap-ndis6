2971cad8fe97e1ee5fbe57c458fb7795774d8bb0
Mikhail Burilov <burilovmv@gmail.com>
2017-07-26 09:40:57 +0700

    Driver installer

---
+++ src/DriverInstaller/DriverInstaller.cpp
@@ -0,0 +1,234 @@
+// DriverInstaller.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <winsock2.h>
+#include <windows.h>
+#include <wincrypt.h>
+#include <wininet.h>
+#include <Wbemidl.h>
+#include <comdef.h>
+#include <Mshtmhst.h>
+#include <shlobj.h>
+#include <commctrl.h>
+#include <Dbghelp.h>
+#include <iphlpapi.h>
+#include <Natupnp.h>
+#include <devguid.h>
+#include <regstr.h>
+#include <cfgmgr32.h>
+#include <tchar.h>
+#include <objbase.h>
+#include <Setupapi.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
+#include <stdarg.h>
+#include <time.h>
+#include <errno.h>
+
+#include "netcfgn.h"
+
+bool InstallNdisProtocolDriver(char *inf_path, UINT lock_timeout)
+{
+	bool ret = false;
+	HRESULT hr;
+	INetCfg *pNetCfg;
+	// Validate arguments
+	if (inf_path == NULL)
+	{
+		return false;
+	}
+	hr = CoCreateInstance(CLSID_CNetCfg, NULL, CLSCTX_INPROC_SERVER, IID_INetCfg, (void **)&pNetCfg);
+
+	if (SUCCEEDED(hr))
+	{
+		INetCfgLock *pLock;
+
+		hr = pNetCfg->QueryInterface(IID_INetCfgLock, (PVOID*)&pLock);
+
+		if (SUCCEEDED(hr))
+		{
+			LPWSTR locked_by;
+
+			hr = pLock->AcquireWriteLock(lock_timeout, L"SoftEther VPN", &locked_by);
+
+			if (SUCCEEDED(hr))
+			{
+				hr = pNetCfg->Initialize(NULL);
+
+				if (SUCCEEDED(hr))
+				{
+					char inf_dir[MAX_PATH];
+					strcpy_s(inf_dir, MAX_PATH, inf_path);
+					char* next = strchr(inf_dir, '\\');
+					while(next)
+					{
+						char* tmp = strchr(next + 1, '\\');
+						if(!tmp)
+						{
+							next[0] = 0;
+							break;
+						}
+						next = tmp;
+					}
+
+					if (SetupCopyOEMInfA(inf_path, inf_dir, SPOST_PATH, 0, NULL, 0, NULL, 0))
+					{
+						INetCfgClassSetup *pSetup;
+
+						hr = pNetCfg->QueryNetCfgClass(&GUID_DEVCLASS_NETTRANS, IID_INetCfgClassSetup, (void **)&pSetup);
+
+						if (SUCCEEDED(hr))
+						{
+							OBO_TOKEN token;
+							INetCfgComponent *pComponent;
+
+							ZeroMemory(&token, sizeof(token));
+
+							token.Type = OBO_USER;
+
+							hr = pSetup->Install(L"AegisPcap", &token, 0, 0, NULL, NULL, &pComponent);
+
+							if (SUCCEEDED(hr))
+							{
+								pNetCfg->Apply();
+
+								ret = true;
+							}
+
+							pSetup->Release();
+						}
+
+						if (ret == false)
+						{
+							char dst_inf_name[MAX_PATH];
+							DWORD dst_inf_name_size = MAX_PATH;
+
+							if (SetupCopyOEMInfA(inf_path, inf_dir, SPOST_PATH, SP_COPY_REPLACEONLY,
+								dst_inf_name, dst_inf_name_size, &dst_inf_name_size, NULL) == false &&
+								GetLastError() == ERROR_FILE_EXISTS)
+							{
+								SetupUninstallOEMInfA(dst_inf_name, 0, NULL);
+							}
+						}
+					}
+				}
+
+				pLock->ReleaseWriteLock();
+			}
+
+			pLock->Release();
+		}
+
+		pNetCfg->Release();
+	}
+
+	return ret;
+}
+
+bool UninstallNdisProtocolDriver(UINT lock_timeout)
+{
+	bool ret = false;
+	HRESULT hr;
+	INetCfg *pNetCfg;
+	// Validate arguments
+	hr = CoCreateInstance(CLSID_CNetCfg, NULL, CLSCTX_INPROC_SERVER, IID_INetCfg, (void **)&pNetCfg);
+
+	if (SUCCEEDED(hr))
+	{
+		INetCfgLock *pLock;
+
+		hr = pNetCfg->QueryInterface(IID_INetCfgLock, (PVOID*)&pLock);
+
+		if (SUCCEEDED(hr))
+		{
+			LPWSTR locked_by;
+
+			hr = pLock->AcquireWriteLock(lock_timeout, L"SoftEther VPN", &locked_by);
+
+			if (SUCCEEDED(hr))
+			{
+				hr = pNetCfg->Initialize(NULL);
+
+				if (SUCCEEDED(hr))
+				{
+					INetCfgComponent *pComponent;
+					hr = pNetCfg->FindComponent(L"AegisPcap", &pComponent);
+
+					if (SUCCEEDED(hr))
+					{
+						INetCfgClassSetup *pSetup;
+
+						hr = pNetCfg->QueryNetCfgClass(&GUID_DEVCLASS_NETTRANS, IID_INetCfgClassSetup, (void **)&pSetup);
+
+						if (SUCCEEDED(hr))
+						{
+							OBO_TOKEN token;
+
+							ZeroMemory(&token, sizeof(token));
+
+							token.Type = OBO_USER;
+
+							hr = pSetup->DeInstall(pComponent, &token, NULL);
+
+							if (SUCCEEDED(hr))
+							{
+								pNetCfg->Apply();
+
+								ret = true;
+							}
+
+							pSetup->Release();
+						}
+					}
+				}
+
+				pLock->ReleaseWriteLock();
+			}
+
+			pLock->Release();
+		}
+
+		pNetCfg->Release();
+	}
+
+	return ret;
+}
+
+
+int main(int argc, char** argv)
+{
+	if(argc>0)
+	{
+		if(!strcmp(argv[0], "/install"))
+		{
+			HMODULE hModule = GetModuleHandle(NULL);
+			char path[MAX_PATH];
+			GetModuleFileName(hModule, path, MAX_PATH);
+			char* last = strchr(path, '\\');
+			while(last)
+			{
+				char* temp = strchr(last + 1, '\\');
+				if(!temp)
+				{
+					last[0] = 0;
+					break;
+				}
+
+				last = temp;
+			}
+
+			InstallNdisProtocolDriver(path, 60 * 1000);
+		} else if (!strcmp(argv[0], "/uninstall"))
+		{
+			UninstallNdisProtocolDriver(60 * 1000);
+		}
+	}
+
+    return 0;
+}
+
