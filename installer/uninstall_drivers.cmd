1c0f4bf77f24f1a06883c9af28d702de718fc26b
Mikhail Burilov <burilovmv@gmail.com>
2017-09-22 12:10:44 +0700

    fixed uninstaller

--- installer/uninstall_drivers.cmd
+++ installer/uninstall_drivers.cmd
@@ -3,22 +3,20 @@ echo WinPCAP NDIS6.0 driver installer
 echo    architecture = %PROCESSOR_ARCHITECTURE% 
 echo    system dir = %SystemRoot%
 
+echo Removing files
+
+del /f /q %SystemRoot%\system32\packet-ndis6.dll
+del /f /q %SystemRoot%\system32\wpcap.dll
+del /f /q %SystemRoot%\SysWOW64\packet-ndis6.dll
+del /f /q %SystemRoot%\SysWOW64\wpcap.dll
+
 if %PROCESSOR_ARCHITECTURE%  == x86 goto x86
 :x64
 echo Uninstalling drivers
 x64\DriverInstaller.exe /uninstall
 
-echo Removing files
-
-copy /y x64\packet-ndis6.dll %SystemRoot%\system32
-copy /y x64\wpcap.dll %SystemRoot%\system32
-copy /y x86\packet-ndis6.dll %SystemRoot%\SysWOW64
-copy /y x86\wpcap.dll %SystemRoot%\SysWOW64
-
-
-copy /y x64\pcap-ndis6.* %SystemRoot%\system32
-
 goto exit
+
 :x86
 echo Uninstalling drivers
 x64\DriverInstaller.exe /uninstall
