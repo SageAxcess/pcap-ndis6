911058f388dc6bac81fba1f35ccc1701a4e47272
Andrey Fedorinin <andrey.fedorinin.wrk@gmail.com>
2018-01-12 03:34:17 +0200

    copyright block for KmThreads.c

--- src/driver/KmThreads.c
+++ src/driver/KmThreads.c
@@ -1,3 +1,16 @@
+//////////////////////////////////////////////////////////////////////
+// Project: pcap-ndis6
+// Description: WinPCAP fork with NDIS6.x support 
+// License: MIT License, read LICENSE file in project root for details
+//
+// Copyright (c) 2017 ChangeDynamix, LLC
+// All Rights Reserved.
+// 
+// https://changedynamix.io/
+// 
+// Author: Andrey Fedorinin
+//////////////////////////////////////////////////////////////////////
+
 #include "..\shared\CommonDefs.h"
 
 #include <ntddk.h>
