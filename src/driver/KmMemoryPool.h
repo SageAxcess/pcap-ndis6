f1fa6cdb53a8c33f48d174fdc936d5c8cfb47dfd
Andrey Fedorinin <andrey.fedorinin.wrk@gmail.com>
2018-02-26 02:18:52 +0200

    Added kernel-mode memory pool

---
+++ src/driver/KmMemoryPool.h
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////////////////////////
+// Project: pcap-ndis6
+// Description: WinPCAP fork with NDIS6.x support 
+// License: MIT License, read LICENSE file in project root for details
+//
+// Copyright (c) 2018 ChangeDynamix, LLC
+// All Rights Reserved.
+// 
+// https://changedynamix.io/
+// 
+// Author: Andrey Fedorinin
+//////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include "KmMemoryManager.h"
+
+NTSTATUS __stdcall Km_MP_Initialize(
+    __in    PKM_MEMORY_MANAGER  MemoryManager,
+    __in    ULONG               BlockSize,
+    __in    ULONG               InitialBlockCount,
+    __in    BOOLEAN             FixedSize,
+    __out   PHANDLE             InstanceHandle);
+
+NTSTATUS __stdcall Km_MP_Finalize(
+    __in    HANDLE  Instance);
+
+NTSTATUS __stdcall Km_MP_Allocate(
+    __in    HANDLE  Instance,
+    __out   PVOID   *Block);
+
+NTSTATUS __stdcall Km_MP_Release(
+    __in    PVOID   Block);
