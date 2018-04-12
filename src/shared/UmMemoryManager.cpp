e167d31f2b53210d5eaa95b5f11f036a7c29336c
Andrey Fedorinin <andrey.fedorinin.wrk@gmail.com>
2018-04-12 19:11:47 +0300

    Added headers (comments)

--- src/shared/UmMemoryManager.cpp
+++ src/shared/UmMemoryManager.cpp
@@ -1,3 +1,15 @@
+//////////////////////////////////////////////////////////////////////
+// Project: pcap-ndis6
+// Description: WinPCAP fork with NDIS6.x support 
+// License: MIT License, read LICENSE file in project root for details
+//
+// Copyright (c) 2018 ChangeDynamix, LLC
+// All Rights Reserved.
+// 
+// https://changedynamix.io/
+// 
+// Author: Andrey Fedorinin
+//////////////////////////////////////////////////////////////////////
 #include "UmMemoryManager.h"
 
 #include "..\shared\CommonDefs.h"
